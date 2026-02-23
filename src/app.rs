43a476d1b40d257920ed4d5d16156e2f400af14d
richerfu <southorange0929@foxmail.com>
2026-02-23 22:43:33 +0800

    fix: basic style

--- src/app.rs
+++ src/app.rs
@@ -1,9 +1,21 @@
-use gpui::{div, prelude::*, Context, IntoElement, Window};
+use gpui::{div, prelude::*, rgb, Context, IntoElement, Window};
 
 pub struct HelloView;
 
 impl Render for HelloView {
     fn render(&mut self, _window: &mut Window, _cx: &mut Context<Self>) -> impl IntoElement {
-        div().size_full().child("Hello GPUI on OHOS (Rust)")
+        div()
+            .size_full()
+            .bg(rgb(0x0b1220))
+            .text_color(rgb(0xffffff))
+            .flex()
+            .justify_center()
+            .items_center()
+            .child(
+                div()
+                    .text_xl()
+                    .font_weight(gpui::FontWeight::BOLD)
+                    .child("Hello GPUI on OHOS (Rust)"),
+            )
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use gpui::{App, Application, WindowOptions};
+use gpui::{px, size, App, AppContext, Application, Bounds, WindowBounds, WindowOptions};
 use log::LevelFilter;
 use ohos_hilog_binding::log::Config;
 use openharmony_ability::OpenHarmonyApp;
@@ -9,12 +9,21 @@ mod app;
 pub fn openharmony_app(app: OpenHarmonyApp) {
     ohos_hilog_binding::log::init_once(Config::default().with_max_level(LevelFilter::Info));
 
+    let inner_app = app.clone();
     Application::with_platform(gpui_platform::current_platform(false))
         .with_ohos_app(app)
-        .run(|cx: &mut App| {
-            cx.open_window(WindowOptions::default(), |_window, cx| {
-                cx.new(|_| app::HelloView)
-            })
+        .run(move |cx: &mut App| {
+            let info = inner_app.content_rect();
+            let default_size = size(px(info.width as f32), px(info.height as f32));
+            let bounds = Bounds::centered(None, default_size, cx);
+
+            cx.open_window(
+                WindowOptions {
+                    window_bounds: Some(WindowBounds::Windowed(bounds)),
+                    ..Default::default()
+                },
+                |_window, cx| cx.new(|_| app::HelloView),
+            )
             .expect("failed to open GPUI window");
 
             cx.activate(true);
